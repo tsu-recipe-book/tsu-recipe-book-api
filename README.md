59f24161589fcbe58b0b3fa6466e45e6d4c9aad2
Nikita Lugachev <nikitalugachev149th@gmail.com>
2026-05-08 14:38:43 +0700

    Add initial Gradle + Spring Boot project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tsu-recipe-book-api
-RESTful backend for the Recipe Book app. Manages products and dishes: create, edit, delete and filter records. Supports automatic nutrition calculation (calories, proteins, fats, carbs), ingredient validation, and dish categorization via macros.
+
+RESTful backend for the Recipe Book app. Manages products and dishes: create, edit, delete and filter records. Supports
+automatic nutrition calculation (calories, proteins, fats, carbs), ingredient validation, and dish categorization via
+macros.
---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ru.nu1ts.recipebook"
+version = "0.0.1-SNAPSHOT"
+description = "tsu-recipe-book-api"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testCompileOnly("org.projectlombok:lombok")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
